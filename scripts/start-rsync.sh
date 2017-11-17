522938f1246e08ae1c2981eba5a14d11c011f168
Locez <loki.a@live.cn>
2017-11-17 21:11:25 +0800

    delete log files && add processes control

--- scripts/start-rsync.sh
+++ scripts/start-rsync.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 # Author Locez
 # Date 2017-01-28
+# Update 2017-11-17
 
 BASE_DIR="$(cd "$(dirname "$0")";cd ..;pwd)"
 
 # 发行版配置目录
 CONFIG_PATH="${BASE_DIR}/config"
-
-# 执行结果
-export RESULT="${BASE_DIR}/result"
 
 # PID进程
 LOCK_FILE="/var/run/mirrors.pid"
@@ -16,8 +14,23 @@ LOCK_FILE="/var/run/mirrors.pid"
 # 存储路径
 MIRRORS="/mirrors"
 
+# 执行结果
+export RESULT="${MIRRORS}/result"
+
+THREAD_NUM=5
 
 PID=$$
+
+MIRRORS_FIFO=${PID}.fifo
+
+mkfifo ${MIRRORS_FIFO}
+exec 7<>${MIRRORS_FIFO}
+rm -rf ${MIRRORS_FIFO}
+
+for (i=1; i<=${THREAD_NUM}; i++)
+do
+    echo >&7
+done
 
 if [ -f ${LOCK_FILE} ]; then
     echo "syncing"
@@ -31,12 +44,18 @@ echo "start rsync at $(date +"%Y-%m-%d %H:%M:%S")" > ${RESULT}
 Env_List=$(ls "${CONFIG_PATH}")
 for distroENV in ${Env_List}
 do
-    source  ${CONFIG_PATH}/${distroENV}
-    if [ ! -f ${LOG_PATH} ]; then
-        mkdir -p ${LOG_PATH}
-    fi
-    ${BASE_DIR}/scripts/rsync-script.sh &
+    read -u7
+    {
+   	 source  ${CONFIG_PATH}/${distroENV}
+   	 if [ ! -f ${LOG_PATH} ]; then
+		mkdir -p ${LOG_PATH}
+	 fi
+	 ${BASE_DIR}/scripts/rsync-script.sh
+	 echo >&7
+    }&
 done
 wait
+exec 7>&-
+exec 7<&-
 rm -rf ${LOCK_FILE}
 exit 0
