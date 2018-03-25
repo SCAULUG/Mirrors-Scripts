0c67cf92a525f2497d1b9037acaf98220ecd0c51
Locez <loki.a@live.cn>
2018-03-25 12:14:15 +0800

    Update rsync-script.sh

--- scripts/rsync-script.sh
+++ scripts/rsync-script.sh
@@ -3,6 +3,6 @@
 # called by start-rsync.sh
 
 printf "<p id=\"%s\">Synchronizing</p>\n" ${DISTRO} >> ${RESULT}
-rsync -avrtH --delete ${UP_STREAM} $LOCAL_PATH > /dev/null
+rsync -avrtH --delete ${UP_STREAM} ${LOCAL_PATH} > /dev/null
 sed -i "s/\(${DISTRO}.*\)Synchronizing/\1Synchronized - $(date +"%Y-%m-%d %H:%M:%S")/g" ${RESULT}
 exit 0
