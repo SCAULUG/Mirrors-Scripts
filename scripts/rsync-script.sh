4aafcd918895b710ac2eec3ef80c415193726d24
imquanquan <imquanquan99@gmail.com>
2018-04-06 23:51:05 +0800

    add error retry and log files

--- scripts/rsync-script.sh
+++ scripts/rsync-script.sh
@@ -2,7 +2,30 @@
 
 # called by start-rsync.sh
 
+# 最大尝试次数
+MAX_RETRIES=5
+
+# 当前同步次数
+sync_times=0
+
+echo "Rsync starts at $(date +"%Y-%m-%d %H:%M:%S")" > ${LOG_FILE}
 printf "<p id=\"%s\">Synchronizing</p>\n" ${DISTRO} >> ${RESULT}
-rsync -avrtH --delete ${UP_STREAM} ${LOCAL_PATH} > /dev/null
+echo "--------------------------------------------------" >> ${LOG_FILE}
+
+rsync -avrtH --progress --delay-updates --delete-after --timeout=${TIME_OUT} ${UP_STREAM} ${LOCAL_PATH} >> ${LOG_FILE} 2>&1
+while [ $? -ne 0 -a $i -lt $MAX_RETRIES ]
+do
+  echo "Retrying ${sync_times} times" >> ${LOG_FILE}
+  rsync -avrtH --progress --delay-updates --delete-after --timeout=${TIME_OUT} ${UP_STREAM} ${LOCAL_PATH} >> ${LOG_FILE} 2>&1
+done
+
+if [ $sync_times -eq $MAX_RETRIES ]
+then
+  echo "Hit maximum number of retries, giving up" >> ${LOG_FILE}
+  sed -i "s/\(${DISTRO}.*\)Synchronizing/\1Synchronized - $(date +"%Y-%m-%d %H:%M:%S")/g" ${RESULT}
+  exit 11
+fi
+
+echo "--------------------------------------------------" >> ${LOG_FILE}
 sed -i "s/\(${DISTRO}.*\)Synchronizing/\1Synchronized - $(date +"%Y-%m-%d %H:%M:%S")/g" ${RESULT}
 exit 0
